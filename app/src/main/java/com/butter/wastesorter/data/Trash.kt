434d93723c8a0d72fd5948fbb22d2b91fc8b99c1
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-11-09 22:42:30 +0900

    [ADD] Trash Code 추가

--- app/src/main/java/com/butter/wastesorter/data/Trash.kt
+++ app/src/main/java/com/butter/wastesorter/data/Trash.kt
@@ -1,3 +1,12 @@
 package com.butter.wastesorter.data
 
-data class Trash(val name: String)
+data class Trash(val name: String, val code: Int) {
+    companion object {
+        const val PLASTIC: Int = 1
+        const val PAPER: Int = 2
+        const val CARDBOARD: Int = 3
+        const val CAN: Int = 4
+        const val GLASS: Int = 5
+        const val METAL: Int = 6
+    }
+}
