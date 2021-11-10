7bc83ee02ad578dc64e4f10e21ec390323e6d7f4
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-11-10 10:40:02 +0900

    [REFACTOR] 모든 변수가 public으로 수정됨

--- app/src/main/java/com/butter/wastesorter/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/butter/wastesorter/viewmodel/MainViewModel.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.butter.wastesorter.R
 import com.butter.wastesorter.data.Trash
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
@@ -15,35 +16,16 @@ import java.io.ByteArrayOutputStream
 
 class MainViewModel : ViewModel() {
 
-    private val trash: MutableLiveData<ArrayList<Trash>> = MutableLiveData()
+    val trash: MutableLiveData<ArrayList<Trash>> = MutableLiveData()
 
-    private val selectedTrash: MutableLiveData<Int> = MutableLiveData()
+    val selectedTrash: MutableLiveData<Int> = MutableLiveData()
 
-    private val imageBitmap: MutableLiveData<Bitmap> = MutableLiveData()
+    val imageBitmap: MutableLiveData<Bitmap> = MutableLiveData()
 
-    fun init() {
-        val list: ArrayList<Trash> = ArrayList()
-        trash.value = list
+    fun init(context: Context) {
+        trash.value = getInitTrashList(context)
         selectedTrash.value = Trash.PLASTIC
     }
-
-    fun setTrash(trash: ArrayList<Trash>) {
-        this.trash.value = trash
-    }
-
-    fun getTrash(): ArrayList<Trash>? = trash.value
-
-    fun setSelectedTrash(trashCode: Int) {
-        this.selectedTrash.value = trashCode
-    }
-
-    fun getSelectedTrash(): Int? = selectedTrash.value
-
-    fun setImageBitmap(imageBitmap: Bitmap) {
-        this.imageBitmap.value = imageBitmap
-    }
-
-    fun getImageBitmap(): Bitmap? = imageBitmap.value
 
     fun uploadImage(): Boolean {
         if (imageBitmap.value != null) {
@@ -65,5 +47,109 @@ class MainViewModel : ViewModel() {
         }
 
         return true
+    }
+
+    fun getInitTrashList(context: Context): ArrayList<Trash> {
+        val list: ArrayList<Trash> = ArrayList()
+
+        context.apply {
+            val plastic: Trash = Trash(
+                "플라스틱",
+                Trash.PLASTIC,
+                arrayListOf(
+                    getString(R.string.info_plastic_way_1),
+                    getString(R.string.info_plastic_way_2)
+                ),
+                arrayListOf(
+                    getString(R.string.info_plastic_tip_1),
+                    getString(R.string.info_plastic_tip_2),
+                    getString(R.string.info_plastic_tip_3)
+                )
+            )
+            list.add(plastic)
+
+            val paper: Trash = Trash(
+                "종이",
+                Trash.PAPER,
+                arrayListOf(
+                    getString(R.string.info_paper_way_1),
+                    getString(R.string.info_paper_way_2),
+                    getString(R.string.info_paper_way_3),
+                    getString(R.string.info_paper_way_4),
+                    getString(R.string.info_paper_way_5)
+                ),
+                arrayListOf(
+                    getString(R.string.info_paper_tip_1),
+                    getString(R.string.info_paper_tip_2)
+                )
+            )
+            list.add(paper)
+
+            val cardboard: Trash = Trash(
+                "상자",
+                Trash.CARDBOARD,
+                arrayListOf(
+                    getString(R.string.info_cardboard_way_1),
+                    getString(R.string.info_cardboard_way_2),
+                    getString(R.string.info_cardboard_way_3),
+                    getString(R.string.info_cardboard_way_4)
+                ),
+                arrayListOf(
+                    getString(R.string.info_cardboard_tip_1),
+                    getString(R.string.info_cardboard_tip_2)
+                )
+            )
+            list.add(cardboard)
+
+            val can: Trash = Trash(
+                "캔",
+                Trash.CAN,
+                arrayListOf(
+                    getString(R.string.info_can_way_1),
+                    getString(R.string.info_can_way_2),
+                    getString(R.string.info_can_way_3),
+                    getString(R.string.info_can_way_4),
+                    getString(R.string.info_can_way_5)
+                ),
+                arrayListOf(
+                    getString(R.string.info_can_tip_1)
+                )
+            )
+            list.add(can)
+
+            val glass: Trash = Trash(
+                "유리",
+                Trash.GLASS,
+                arrayListOf(
+                    getString(R.string.info_glass_way_1),
+                    getString(R.string.info_glass_way_2),
+                    getString(R.string.info_glass_way_3),
+                    getString(R.string.info_glass_way_4),
+                    getString(R.string.info_glass_way_5),
+                    getString(R.string.info_glass_way_6)
+                ),
+                arrayListOf(
+                    getString(R.string.info_glass_tip_1),
+                    getString(R.string.info_glass_tip_2),
+                    getString(R.string.info_glass_tip_3)
+                )
+            )
+            list.add(glass)
+
+            val metal: Trash = Trash(
+                "철",
+                Trash.METAL,
+                arrayListOf(
+                    getString(R.string.info_metal_way_1),
+                    getString(R.string.info_metal_way_2)
+                ),
+                arrayListOf(
+                    getString(R.string.info_metal_tip_1)
+                )
+            )
+            list.add(metal)
+        }
+
+        return list
     }
 }
