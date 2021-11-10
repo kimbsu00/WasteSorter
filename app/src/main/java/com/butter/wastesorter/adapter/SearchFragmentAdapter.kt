441602941dcb6f6e4fbd0f8b5713cc3877506244
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-11-10 10:41:32 +0900

    [ADD] Filterable Interface 구현

--- app/src/main/java/com/butter/wastesorter/adapter/SearchFragmentAdapter.kt
+++ app/src/main/java/com/butter/wastesorter/adapter/SearchFragmentAdapter.kt
@@ -3,13 +3,15 @@ package com.butter.wastesorter.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.butter.wastesorter.R
 import com.butter.wastesorter.data.Trash
 
 class SearchFragmentAdapter(var items: ArrayList<Trash>, var filteredItems: ArrayList<Trash>) :
-    RecyclerView.Adapter<SearchFragmentAdapter.ViewHolder>() {
+    RecyclerView.Adapter<SearchFragmentAdapter.ViewHolder>(), Filterable {
 
     var listener: OnItemClickListener? = null
 
@@ -41,5 +43,37 @@ class SearchFragmentAdapter(var items: ArrayList<Trash>, var filteredItems: Arra
 
     override fun getItemCount(): Int {
         return filteredItems.size
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val str: String = constraint.toString()
+
+                if (str.isBlank()) {
+                    filteredItems = items
+                } else {
+                    val filteringList: ArrayList<Trash> = ArrayList()
+                    for (trash in items) {
+                        if (trash.name.contains(str)) {
+                            filteringList.add(trash)
+                        }
+                    }
+                    filteredItems = filteringList
+                }
+                val result: FilterResults = FilterResults()
+                result.values = filteredItems
+
+                return result
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                if (results != null) {
+                    filteredItems = results.values as ArrayList<Trash>
+                    notifyDataSetChanged()
+                }
+            }
+
+        }
     }
 }
